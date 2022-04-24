ebd091b65ca2a7df6e2dcbf14bbb6375826de68d
SALEH ALAOUDI <79456369+salaoudi@users.noreply.github.com>
2022-04-24 06:57:44 -0700

    Initial Commit

---
+++ quoteplanet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quoteplanet
+//
+//  Created by Zayn on 4/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quoteplanet/quoteplanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  quoteplanetApp.swift
+//  quoteplanet
+//
+//  Created by Zayn on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct quoteplanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
