32a6b4ea96eee7caa4e54b4523cfdf4a31d5495e
SALEH ALAOUDI <79456369+salaoudi@users.noreply.github.com>
2022-04-25 18:39:34 -0700

    cleanup code

--- quoteplanet/Service/NetworkManager.swift
+++ quoteplanet/Service/NetworkManager.swift
@@ -1,9 +1,4 @@
-//
-//  NetworkManager.swift
-//  quoteplanet
-//
-//  Created by Zayn on 4/25/22.
-//
+
 
 import Foundation
 
@@ -25,7 +20,7 @@ class NetworkManager {
 	
 	func request<T: Decodable>(fromURL url: URL, httpMethod: HttpMethod = .get, completion: @escaping (Result<T, Error>) -> Void) {
 		
-		let completionOnMain: (Result<T, Error>) -> Void = { result in
+		let complete: (Result<T, Error>) -> Void = { result in
 			DispatchQueue.main.async {
 				completion(result)
 			}
@@ -35,31 +30,31 @@ class NetworkManager {
 		var request = URLRequest(url: url)
 		request.httpMethod = httpMethod.method
 
-		let urlSession = URLSession.shared.dataTask(with: request) { data, response, error in
+		let session = URLSession.shared.dataTask(with: request) { data, response, error in
 			
 			if let error = error {
-				completionOnMain(.failure(error))
+				complete(.failure(error))
 				return
 			}
 
 		
-			guard let urlResponse = response as? HTTPURLResponse else { return completionOnMain(.failure(ManagerErrors.invalidResponse)) }
+			guard let urlResponse = response as? HTTPURLResponse else { return complete(.failure(ManagerErrors.invalidResponse)) }
 			if !(200..<300).contains(urlResponse.statusCode) {
-				return completionOnMain(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
+				return complete(.failure(ManagerErrors.invalidStatusCode(urlResponse.statusCode)))
 			}
 
 			
 			guard let data = data else { return }
 			do {
 				let all = try JSONDecoder().decode(T.self, from: data)
-				completionOnMain(.success(all))
+				complete(.success(all))
 			} catch {
-				debugPrint("Could not translate the data to the requested type. Reason: \(error.localizedDescription)")
-				completionOnMain(.failure(error))
+				print("Could not translate the data to the requested type. Reason: \(error.localizedDescription)")
+				complete(.failure(error))
 			}
 		}
 
 		
-		urlSession.resume()
+		session.resume()
 	}
 }
